90b7b9c0dbd9bca78faad38897ca8d6e627326d1
Pedro Marsao <pedro.marsao@gmail.com>
2025-12-13 19:32:20 -0300

    Componentes

---
+++ src/app/settings/user/page.tsx
@@ -0,0 +1,28 @@
+import { getAppSettings, getUserInfo } from "@/app/lib/settings";
+import React from "react";
+
+
+async function UserSettings() {
+    const {name, email, age} = await getUserInfo();
+    return (
+        <div>
+            <h1 className="text-2xl font-bold">User Page</h1>
+
+            <div 
+                className="
+                border border-dashed 
+                border-red-500
+                p-4"
+            >
+
+                <p>Name: {name}</p>
+                <p>Email: {email}</p>
+                <p>Age: {age}</p>
+
+            </div>
+        </div>
+    );
+}
+
+
+export default UserSettings;
