d6c024d3b33db4472a938ae095e7ea9453804d9c
Pedro Marsao <pedro.marsao@gmail.com>
2025-12-13 16:27:58 -0300

    Modulo 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import Header from './components/Header';
 import { MovieRow } from './components/MovieRow';
 import { Banner } from './components/Banner';
@@ -6,9 +5,9 @@ import { Banner } from './components/Banner';
 
 export default function Home() {
   return (
-    <div className='relative h-screen overflow-hidden bg-gradient-to-b lg:h-[140vh] scrollbar-hide'>
+    <div className='relative h-screen overflow-hidden bg-gradient-to-b lg:h-[140vh]'>
       <Header />
-      <main className='relative pb-24 pl-4 lg:pl-16 scrollbar-hide'>
+      <main className='relative pb-24 pl-4 lg:pl-16'>
         <Banner />
         <MovieRow sessionTitle='Trending Now' />
         <MovieRow sessionTitle='Top Rated' />
